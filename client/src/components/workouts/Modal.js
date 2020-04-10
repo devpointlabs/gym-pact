823f6ff0f4bd5f12c7384d00479e7fc9f456249a
jcoops22 <jcooper232074@gmail.com>
2020-04-09 22:02:39 -0600

    looks more like the wireframe

--- client/src/components/workouts/Modal.js
+++ client/src/components/workouts/Modal.js
@@ -3,14 +3,11 @@ import styled from "styled-components";
 import ropesImg from "../../imgs/ropes.jpg";
 
 const ModalDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 30%;
   z-index: -1;
   cursor: pointer;
 `;
 const Background = styled.div`
+  transition-duration: 1s;
   position: fixed;
   bottom: 0vh;
   right: 0vw;
@@ -23,49 +20,79 @@ const Background = styled.div`
 `;
 const Container = styled.div`
   position: fixed;
+  top: 15vh;
+  left: 20vw;
+  background-color: #eee;
+  border-radius: 5px;
+  z-index: 3;
+  /* width: 70vw; */
+`;
+const Column = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  width: 20vw;
 `;
-const Header = styled.h2`
-  color: orange;
-  position: fixed;
+const Row = styled.div`
+  display: flex;
+`;
+const H1 = styled.h1`
+  position: relative;
   text-align: center;
-  top: 20vh;
   font-size: 35px;
   z-index: 12;
 `;
-const Text = styled.p`
-  position: fixed;
+const H2 = styled.h2`
+  position: relative;
   text-align: center;
-  top: 40vh;
   font-size: 25px;
-  color: orange;
+  z-index: 12;
+`;
+const Desc = styled.p`
+  position: relative;
+  text-align: center;
+  min-height: 10vh;
+  font-size: 20px;
   z-index: 12;
 `;
 const Image = styled.img`
-  position: fixed;
-  top: 0;
-  width: 50vw;
+  width: 48vw;
+  height: 70vh;
   z-index: 2;
 `;
 
 const Modal = (props) => {
   const [display, setDisplay] = useState("none");
-  const toggle = (e) => {
+  const show = (e) => {
     if (display == "none") {
       setDisplay("block");
       document.body.style.overflowY = "hidden";
-    } else {
+    }
+  };
+  const hide = () => {
+    if (display == "block") {
       setDisplay("none");
       document.body.style.overflowY = "initial";
     }
   };
   return (
-    <ModalDiv onClick={toggle}>
-      <Background style={{ display: display }}></Background>
-      <Image style={{ display: display }} src={ropesImg} />
-      <Header style={{ display: display }}>{props.workout.title}</Header>
-      <Text style={{ display: display }}>{props.workout.desc}</Text>
+    <ModalDiv onClick={show}>
+      <Background onClick={hide} style={{ display: display }}></Background>
+      <Container style={{ display: display }}>
+        <Row>
+          <Image src={ropesImg} />
+          <Column>
+            <H2>User Name</H2>
+            <p>{props.workout.created_at}</p>
+            <H1>Title: {props.workout.title}</H1>
+            <Desc>Workout: {props.workout.desc}</Desc>
+            <p>(number of) Comments</p>
+            <Desc>
+              <p>Enter Comments Here</p>
+            </Desc>
+          </Column>
+        </Row>
+      </Container>
       {props.children}
     </ModalDiv>
   );
