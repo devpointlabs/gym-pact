5845a17d2a15bbcc6c7e2a6ab4aa615cd4aa5227
jcoops22 <jcooper232074@gmail.com>
2020-04-14 00:07:52 -0600

    correct minor issues to get app running

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,17 +3,12 @@ import { Header } from "semantic-ui-react";
 import ConnectedFeedWorkouts from "./workouts/FeedWorkouts";
 
 const Home = () => (
-
   <>
-  // these two headers were from merge conflict
-      <Header textAlign='center'>GYM PACT</Header>
+    // these two headers were from merge conflict
+    <Header textAlign="center">GYM PACT</Header>
     <Header textAlign="center">Feed</Header>
     <ConnectedFeedWorkouts className="feed" />
   </>
 );
-
-
-)
-
 
 export default Home;
