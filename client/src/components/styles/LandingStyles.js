5df46dfb90223169f74842ecc773c287f4207e80
Carson Meiling <carmc93@gmail.com>
2020-04-14 17:56:18 -0600

    icons

--- client/src/components/styles/LandingStyles.js
+++ client/src/components/styles/LandingStyles.js
@@ -6,21 +6,26 @@ export const Landing = styled.div`
 display: grid;
 grid-template-columns: 50% 50%;
 height: 100vh;
-width: 100vw;
 `
 
 export const BackgroundLeft = styled.div`
 background-color: #F0F1F8;
+
 `
 export const Right = styled.div`
 background-image: url(${gym});
+background-size: cover;
+background-position: center;
+background-repeat: no-repeat;
+height: 100%;
 `
 export const Left = styled.div`
 background-color: #F0F1F8;
 `
 export const Container = styled.div`
 padding-top: 10%;
-text-align: center
+text-align: left;
+margin-left: 13%;
 `
 export const MainFont = styled.h1`
 color: #FBD878;
@@ -29,17 +34,3 @@ font-size: 5rem;
 export const CallBtn = styled.button`
 
 `
-// import { 
-//    Background,
-//    Container,
-//    MainFont,
-//    CallBtn,
-//  } from './styles/LandingStyles';
-
-{/* <Background>
-    <Container>
-       <MainFont>GYM PACT</MainFont>
-       <CallBtn>Register</CallBtn>
-       <CallBtn>Login</CallBtn>
-    </Container>
- </Background> */}
