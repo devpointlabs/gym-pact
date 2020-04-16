73392f6f735f0b45b53f42ba008474e31c6027b4
Carson Meiling <carmc93@gmail.com>
2020-04-15 18:33:29 -0600

    fixes

--- client/src/components/comments/Comment.js
+++ client/src/components/comments/Comment.js
@@ -3,12 +3,27 @@ import axios from 'axios';
 
 
 class Comment extends Component {
+   state = {comments: []}
 
-   componentDidMount(){
-      axios.get()
+   componentDidMount(comment, user_id, workout_id){
+      axios.get(`/api/workouts/${id}/comments`, comment)
+         .then( res => {
+            const { comment } = this.state
+            this.setState({...comments, [res.data]})
+         })
+         .catch( err => {
+            console.log(err)
+         })
    }
+
+
    render(){
+      const { comments } = this.state
       return(
+
+         <>
+            {
+         </>
          
       )
    }
