5dd0c25961629d8784c1e5b5fa7b0dbe6bb0d7a6
Diane <dianenguyen9521175@gmail.com>
2020-04-23 17:51:42 -0600

    edited user profile page, added cards for following followers information, and your workouts

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,29 +1,35 @@
-import React from 'react';
+// import React from 'react';
+// import UserStyle from './User/UserStyle.css'
 
-const Footer = () => {
-  return (
-    <div style={styles.footer}>
+// const Footer = () => {
+//   return (
+//     <div style={styles.footer}>
 
-    </div>
-  )
-}
 
-export default Footer;
 
-const styles = {
-  footer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyConent: "center",
-    backgroundColor: "#292A4C",
-    position: "fixed",
-    left: "0",
-    bottom: "0",
-    width: "100%",
-    // backgroundColor: "black",
-    // color: "white",
-    textAlign: "center",
-    height: "4em",
-    marginTop: "-8em",
-  },
-};
+//     </div>
+//   )
+// }
+
+// export default Footer;
+
+// const styles = {
+//   footer: {
+//     position: "static",
+//     display: "flex",
+//     flexDirection: "row",
+//     justifyConent: "center",
+//     backgroundColor: "#292A4C",
+//     position: "fixed",
+//     left: "0",
+//     bottom: "0",
+//     width: "100%",
+//     display: "flex; flex- direction: column",
+//     // backgroundColor: "black",
+//     // color: "white",
+
+//     textAlign: "center",
+//     height: "1.5em",
+//     marginTop: "-8em",
+//   },
+// };
